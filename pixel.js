f36e0fad166a1fb6c5c3c33fdace4fe42c0ea14d
keptan <keptan@installgentoo.com>
2018-09-12 23:29:10 +0000

    added image to pixels that takes a url node

--- pixel.js
+++ pixel.js
@@ -1,4 +1,6 @@
 var Matrix = require('node-rpi-rgb-led-matrix')
+var getPixels = require('get-pixels'); 
+
 
 //var led = new LedMatrix(64, 64, 1, 2, "adafruit-hat-pwm");
 
@@ -40,9 +42,47 @@ module.exports = function(RED) {
 		}); 
 	}
 
+	function ImageToPixels (config) 
+	{
+		RED.nodes.createNode(this, config); 
+		var node = this; 
+		node.file = config.file; 
+
+		var output = [];
+
+		function readySend () 
+		{
+			node.send([output]); 
+		}
+
+		node.on('input', function(msg) 
+		{
+			getPixels(node.file, function(err, pixels) 
+			{
+				for(var x = 0; x < 128; x++) 
+				{
+					for(var y = 0; y < 64; y++)
+					{
+						if(pixels.get(x,y,0))
+						{
+							output.push({ x:x, y:y, r:pixels.get(x,y,0), g:pixels.get(x,y,1), b:pixels.get(x,y,2)});
+						}
+					}
+				}
+
+				readySend();
+			});
+
+		});
+	}
+
+
+
+
 
 	RED.nodes.registerType("led-matrix", LedMatrix);
 	RED.nodes.registerType("pixel", PixelNode);
 	RED.nodes.registerType("refresh-matrix", RefreshMatrix);
+	RED.nodes.registerType("image-to-pixels", ImageToPixels);
 }
 			
