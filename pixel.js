b25cad2565056bca29321767869a8ecde8d92095
keptan <keptan@installgentoo.com>
2018-09-19 22:18:38 +0000

    actually use configuration for matrix-config node

--- pixel.js
+++ pixel.js
@@ -19,13 +19,17 @@ module.exports = function(RED) {
 		RED.nodes.createNode(this, n);
 
 		//get the field settings, these inputs are defined in the html 
+		
 		this.width = n.width; 
 		this.height = n.height; 
+		this.chained = n.chained; 
+		this.parallel = n.parallel; 
+		this.mapping = n.mapping; 
 
 		//if led is undefined we create a new one
 		if(!led) 
 		{
-			led = new Matrix(64, 64, 1, 2, "adafruit-hat-pwm");
+			led = new Matrix(this.width, this.height, this.chained, this.parallel, this.mapping);
 		}
 
 		//otherwise we clear the one we have, without these checks it can spawn new evertime we deploy 
@@ -49,6 +53,9 @@ module.exports = function(RED) {
 
 		node.on('input', function(msg) 
 		{ 
+			//console.log("setting");
+			//console.log(msg.payload.x); 
+			//console.log(msg.payload.y);
 			//self documenting 
 			led.setPixel(msg.payload.x, msg.payload.y, msg.payload.r, msg.payload.g, msg.payload.b);
 
@@ -94,12 +101,13 @@ module.exports = function(RED) {
 		//function to actually send the output to the next node 
 		function readySend () 
 		{
-			console.log("sending pixels");
+			//console.log("sending pixels");
 			
 			//see node-red docmentation for how node.send treats arrays 
 			//node.send(output) would send one pixel to n outputs 
 			//node.send([output], true) sends the pixiels to output 1, and true to output 2 
 			node.send([output]); 
+			//console.log("sending pixels");
 		}
 
 		//if we receive input
@@ -126,6 +134,8 @@ module.exports = function(RED) {
 						if(pixels.get(x,y,0))
 						{
 							//push pixels to the output buffer 
+							//console.log(x);
+							//console.log(y);
 							output.push({payload: { x:x, y:y, r:pixels.get(x,y,0), g:pixels.get(x,y,1), b:pixels.get(x,y,2)} });
 						}
 					}
@@ -154,6 +164,23 @@ module.exports = function(RED) {
 	}
 			
 
+	function Text (config) 
+	{
+		RED.nodes.createNode(this, config); 
+		var node = this; 
+
+		node.on('input', function(msg) 
+		{
+			if(msg.payload)
+			{
+				led.textTest(msg.payload, "/home/pi/node-red-contrib-led-matrix/9x18B.bdf"); 
+				led.setPixel(0,0,255,255,255);
+				led.update();
+			}
+		}); 
+	}
+
+
 
 
 
@@ -163,5 +190,6 @@ module.exports = function(RED) {
 	RED.nodes.registerType("pixel", PixelNode);
 	RED.nodes.registerType("refresh-matrix", RefreshMatrix);
 	RED.nodes.registerType("image-to-pixels", ImageToPixels);
+	RED.nodes.registerType("text", Text); 
 }
 			
