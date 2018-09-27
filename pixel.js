1eca830af78a0260167ce656fbd5a9256ad90078
= <=>
2018-09-27 00:11:50 +0000

    working color picker in the data-transform node!

--- pixel.js
+++ pixel.js
@@ -23,6 +23,22 @@ module.exports = function(RED) {
 
 	}
 
+	function eatHexString (hex) 
+	{
+		// Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+		var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+		hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+			return r + r + g + g + b + b;
+		});
+
+		var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+		return result ? {
+			r: parseInt(result[1], 16),
+			g: parseInt(result[2], 16),
+			b: parseInt(result[3], 16)
+		} : null;
+	}
+
 	function LedMatrix(n) 
 	{
 		RED.nodes.createNode(this, n);
@@ -271,6 +287,7 @@ module.exports = function(RED) {
 		node.yOffset = (config.yOffset || 0); 
 		node.refresh = (config.refresh || 0); 
 		node.rgb	 = (config.rgb     || "255,255,255");
+		node.rgb2    = (config.rgb2    || "none");
 
 		function outputFromString (msg) 
 		{
