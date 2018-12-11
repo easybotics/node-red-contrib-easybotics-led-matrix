230c1bec1d4f35830bceb7c2c60e24d567e27207
keptan <keptan@protonmail.com>
2018-12-10 14:15:11 -1000

    import display primitives, and move over the fill test to use them

--- pixel.js
+++ pixel.js
@@ -1,10 +1,12 @@
 var Matrix		= require('easybotics-rpi-rgb-led-matrix')
 var getPixels	= require('get-pixels');
+var dp			= require('./displayPrimitives.js')
 
 
 //var led = new LedMatrix(64, 64, 1, 2, "adafruit-hat-pwm");
 
 module.exports = function(RED) {
+
 
 	var led;
 	var nodeRegister;
@@ -896,73 +898,10 @@ module.exports = function(RED) {
 		node.matrix = RED.nodes.getNode(config.matrix);
 		node.zLevel = 1;
 
-		function Point (x, y)
-		{
-			this.x = x;
-			this.y = y;
-		}
-
-		function Line (start, end)
-		{
-			this.start = start;
-			this.end   = end;
-
-			this.intersects = function (line)
-			{
-				function onSegment (p, q, r)
-				{
-					if (q.x <= Math.max(p.x, r.x) && 
-						q.x >= Math.min(p.x, r.x) && 
-						q.y <= Math.max(p.y, r.y) && 
-						q.y >= Math.min(p.y, r.y)) 
-					{
-						return true; 
-					}
-  
-					return false; 
-				}
-
-				function orientation (p, q, r)
-				{
-					val = (q.y - p.y) * (r.x - q.x) -(q.x - p.x) * (r.y - q.y);
-
-					if (val == 0) return 0;  // colinear
-
-					return (val > 0)? 1: 2; // clock or counterclock wise
-				}
-
-				p1 = this.start;
-				q1 = this.end;
-				p2 = line.start;
-				q2 = line.end;
-
-				o1 = orientation(p1, q1, p2);
-				o2 = orientation(p1, q1, q2);
-				o3 = orientation(p2, q2, p1);
-				o4 = orientation(p2, q2, q1);
-
-				if (o1 != o2 && o3 != o4)
-					return true;
-
-				// Special Cases 
-				// p1, q1 and p2 are colinear and p2 lies on segment p1q1 
-				if (o1 == 0 && onSegment(p1, p2, q1)) return true; 
-			  
-				// p1, q1 and q2 are colinear and q2 lies on segment p1q1 
-				if (o2 == 0 && onSegment(p1, q2, q1)) return true; 
-			  
-				// p2, q2 and p1 are colinear and p1 lies on segment p2q2 
-				if (o3 == 0 && onSegment(p2, p1, q2)) return true; 
-			  
-				 // p2, q2 and q1 are colinear and q1 lies on segment p2q2 
-				if (o4 == 0 && onSegment(p2, q1, q2)) return true; 
-			  
-				return false; // Doesn't fall in any of the above cases 
-			}
-		}
 
 
-		const points = [ new Point(10,10), new Point(20,30), new Point(25, 5)]
+
+		const points = [ new dp.Point(10,10), new dp.Point(20,30),  new dp.Point(25, 5)]
 
 
 		function getLines ()
@@ -974,12 +913,12 @@ module.exports = function(RED) {
 			for(const p of points)
 			{
 				if (last)
-					lines.push(new Line(last, p));
+					lines.push(new dp.Line(last, p));
 
 				last = p;
 			}
 
-			lines.push(new Line(last, first));
+			lines.push(new dp.Line(last, first));
 			return lines;
 		}
 
@@ -1006,7 +945,7 @@ module.exports = function(RED) {
 				y = p.y < y ? p.y : y;
 			}
 
-			return new Point(x, y);
+			return new dp.Point(x, y);
 		}
 
 		function bottomRight ()
@@ -1020,7 +959,7 @@ module.exports = function(RED) {
 				y = p.y > y ? p.y : y;
 			}
 
-			return new Point(x, y);
+			return new dp.Point(x, y);
 		}
 
 		node.draw = function ()
@@ -1041,8 +980,8 @@ module.exports = function(RED) {
 			{
 				for(y = tl.y; y < br.y; y++)
 				{
-					leftTest = new Line( new Point(0, y), new Point(x, y));
-					rightTest = new Line( new Point(x,y), new Point(100, y));
+					leftTest = new dp.Line( new dp.Point(0, y), new dp.Point(x, y));
+					rightTest = new dp.Line( new dp.Point(x,y), new dp.Point(100, y));
 
 					const left  = ins(leftTest);
 					const right = ins(rightTest);
